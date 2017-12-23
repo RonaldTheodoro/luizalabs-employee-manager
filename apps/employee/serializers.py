7476048d84e1efba4ee2a3a287714d5c91e4f012
ronald theodoro <ronald.silva4@fatec.sp.gov.br>
2017-12-23 01:20:45 -0200

    add employee link

--- apps/employee/serializers.py
+++ apps/employee/serializers.py
@@ -3,11 +3,14 @@ from rest_framework import serializers
 from . import models
 
 
-class DepartmentSerializer(serializers.ModelSerializer):
+class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
+    employee = serializers.HyperlinkedIdentityField(
+        view_name='employee-detail'
+    )
 
     class Meta:
         model = models.Department
-        fields = ('id', 'name', )
+        fields = ('id', 'name', 'employee', )
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
