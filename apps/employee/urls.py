eef7c81c1a16a0b437165669ff0ffe8532fd1a7a
ronald theodoro <ronald.silva4@fatec.sp.gov.br>
2017-12-23 00:39:08 -0200

    register departtment routes

--- apps/employee/urls.py
+++ apps/employee/urls.py
@@ -1,5 +1,14 @@
-from django.urls import path
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+from . import views
 
 app_name = 'employee'
 
-urlpatterns = []
+router = DefaultRouter()
+router.register(r'department', views.DepartmentViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
