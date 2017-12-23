6d347c1b22b3c2fe05df8bb51e094256897c7f0d
ronald theodoro <ronald.silva4@fatec.sp.gov.br>
2017-12-23 00:02:33 -0200

    add Employee and Department models with tests

--- apps/employee/models.py
+++ apps/employee/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField('name', max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    name = models.CharField('name', max_length=150)
+    email = models.EmailField('email')
+    department = models.ForeignKey(
+        'Department',
+        verbose_name='department',
+        related_name='employees',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
--- apps/employee/tests.py
+++ apps/employee/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
-# Create your tests here.
+from . import models
+
+
+class DepartmentModelTest(TestCase):
+
+    def setUp(self):
+        self.obj = models.Department.objects.create(name='Architecture')
+
+    def test_create(self):
+        self.assertTrue(models.Department.objects.exists())
+
+    def test_str(self):
+        self.assertEqual('Architecture', str(self.obj))
+
+
+class EmployeeModelTest(TestCase):
+
+    def setUp(self):
+        department = models.Department.objects.create(name='Architecture')
+        self.obj = models.Employee.objects.create(
+            name='Arnaldo Pereira',
+            email='arnaldo@luizalabs.com',
+            department=department
+        )
+
+    def test_create(self):
+        self.assertTrue(models.Employee.objects.exists())
+
+    def test_str(self):
+        self.assertEqual('Arnaldo Pereira', str(self.obj))
