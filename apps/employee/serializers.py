91fe60eb8ab9bcd455c5750c6bbd1ce16aacc0a4
ronald theodoro <ronald.silva4@fatec.sp.gov.br>
2017-12-23 00:51:44 -0200

    add EmployeeSerializer

--- apps/employee/serializers.py
+++ apps/employee/serializers.py
@@ -8,3 +8,11 @@ class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Department
         fields = ('id', 'name', )
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    department = serializers.ReadOnlyField(source='department.name')
+
+    class Meta:
+        model = models.Employee
+        fields = ('id', 'name', 'email', 'department', )
