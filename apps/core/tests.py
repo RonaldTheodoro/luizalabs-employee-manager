f58dc22ca1c667f464dd56b193c7a157d64a4363
ronald theodoro <ronald.silva4@fatec.sp.gov.br>
2017-12-22 23:50:23 -0200

    add index view tests

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexViewTest(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get(reverse('core:index'))
+
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'index.html')
