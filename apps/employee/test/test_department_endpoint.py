169f4bfbdd654f56ae86234d8aabbf5afb130895
ronald theodoro <ronald.silva4@fatec.sp.gov.br>
2017-12-23 18:11:25 -0200

    add DepartmentGetEndPointTest

---
+++ apps/employee/test/test_department_endpoint.py
@@ -0,0 +1,32 @@
+import json
+
+from django.urls import reverse
+
+from rest_framework.test import APITestCase
+
+
+class DepartmentGetEndPointTest(APITestCase):
+    url = reverse('department-list')
+    data = [
+        {'name': 'Architecture'},
+        {'name': 'E-commerce'},
+        {'name': 'Mobile'}
+    ]
+
+    def setUp(self):
+        self.create_data()
+        self.response = self.client.get(self.url)
+
+    def create_data(self):
+        for register in self.data:
+            self.client.post(self.url, register)
+
+    def create_json(self):
+        return json.loads(self.response.rendered_content)
+
+    def test_get_department(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_get_number_of_registers(self):
+        itens = self.create_json()
+        self.assertEqual(3, itens['count'])
