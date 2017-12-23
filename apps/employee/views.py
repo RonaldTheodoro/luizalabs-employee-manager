89fb534ce79c1d56a18343dad036db6313e09e12
ronald theodoro <ronald.silva4@fatec.sp.gov.br>
2017-12-23 00:51:59 -0200

    add EmployeeViewSet

--- apps/employee/views.py
+++ apps/employee/views.py
@@ -6,3 +6,8 @@ from . import models, serializers
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = models.Department.objects.all()
     serializer_class = serializers.DepartmentSerializer
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = models.Employee.objects.all()
+    serializer_class = serializers.EmployeeSerializer
