acad00782613ff26fd008303ebe8c12e6b926aea
ronald theodoro <ronald.silva4@fatec.sp.gov.br>
2017-12-24 15:44:38 -0200

    add permission classes

--- apps/employee/views.py
+++ apps/employee/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from . import models, serializers
 
@@ -7,7 +7,11 @@ class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = models.Department.objects.all()
     serializer_class = serializers.DepartmentSerializer
 
+    permission_classes = (permissions.IsAuthenticated,)
+
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = models.Employee.objects.all()
     serializer_class = serializers.EmployeeSerializer
+
+    permission_classes = (permissions.IsAuthenticated,)
